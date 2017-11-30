ac8cf5396ef114db02a62c513d0fa7b66861f1a4
Herberto Tiago Baptista Cardoso <htbc@dei.student.uc.pt>
2017-11-30 13:04:32 +0000

    teste

--- web/index.php
+++ web/index.php
@@ -1,9 +1,18 @@
+<!DOCTYPE html>
 <html>
 <title>Teste php</title>
 <head></head>
 <body>
-
-
+ola
+<?php
+$conn = pg_connect();
+$conn = pg_connect("host=ec2-184-73-206-155.compute-1.amazonaws.com dbname=dcddreidjnggtn user=agvlrlhnpncwkh password=548d6de5795bf901f45d996069017c9002b8374deada3c37331b2ab74bea0e56");
+if (!$conn) {
+  echo "An error occured.\n";
+  exit; // Para a execução do script
+}else{
+	echo "ligacao aceite";
+}?>
 
 </body>
 </html>
