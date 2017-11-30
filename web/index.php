1dbb249205a65299400b291cddaa66c403dc5d1b
Herberto Tiago Baptista Cardoso <htbc@dei.student.uc.pt>
2017-11-30 12:26:26 +0000

    fix db connect

--- web/index.php
+++ web/index.php
@@ -2,21 +2,7 @@
 <title>Teste php</title>
 <head></head>
 <body>
-<?
-$servidor = 'ec2-184-73-206-155.compute-1.amazonaws.com';
-$banco = 'dcddreidjnggtn';
-$porto = 5432;
-$usuario = 'agvlrlhnpncwkh';
-$senha = '548d6de5795bf901f45d996069017c9002b8374deada3c37331b2ab74bea0e56';
-$string_con = "host=$servidor port=$porto dbname=$banco user=$usuario password=$senha";
-$conn = pg_connect($string_con);
-if(!conn)
-{
-    echo "erro ao conectar ao banco de dados!";}
-else{
-	echo "entrou na data base";
-}
-?>
+
 
 
 </body>
