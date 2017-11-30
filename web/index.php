91f9d7edba5b248de256333686d993c49a0154fe
Herberto Tiago Baptista Cardoso <htbc@dei.student.uc.pt>
2017-11-30 12:24:56 +0000

    fix db connect

--- web/index.php
+++ web/index.php
@@ -3,14 +3,14 @@
 <head></head>
 <body>
 <?
-$servidor = 'ec2-184-73-206-155.compute-1.amazonaws.com:5432';
+$servidor = 'ec2-184-73-206-155.compute-1.amazonaws.com';
 $banco = 'dcddreidjnggtn';
+$porto = 5432;
 $usuario = 'agvlrlhnpncwkh';
 $senha = '548d6de5795bf901f45d996069017c9002b8374deada3c37331b2ab74bea0e56';
-$link = mysql_connect($servidor, $usuario, $senha);
-
-$db = mysql_select_db($banco,$link);
-if(!$link)
+$string_con = "host=$servidor port=$porto dbname=$banco user=$usuario password=$senha";
+$conn = pg_connect($string_con);
+if(!conn)
 {
     echo "erro ao conectar ao banco de dados!";}
 else{
