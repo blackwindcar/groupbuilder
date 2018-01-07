4b722e00a7b5c6b5ceed1caf708d00fded0c29a3
Herberto Tiago Baptista Cardoso <htbc@dei.student.uc.pt>
2018-01-07 19:23:02 +0000

    msn

--- web/newPassword.php
+++ web/newPassword.php
@@ -11,48 +11,47 @@ if (!$conn) {
 
 if($_SESSION["utilizador"]==null or $_SESSION["password"]==null){
 		pg_close($conn);
-		//header("location: login.php");
+		header("location: login.php");
 		
 	}
-echo("secao: ".($_SESSION["utilizador"]==null or $_SESSION["password"]==null));
 
 $user = $_SESSION["utilizador"];
 $pass = $_SESSION["password"];
-echo("</br>user: $user</br>pass: $pass");
+
 $sql = "select count(*) from \"utilizador\" where \"user\" = '$user' and \"password\" = '$pass'";
 if(pg_fetch_row(pg_query($conn,$sql))[0]=="0"){
 	pg_close($conn);
-	//header("location: login.php");
+	header("location: login.php");
 }
-echo("</br>login: ".pg_fetch_row(pg_query($conn,$sql))[0]);
 
 if($_POST["pass"] == null or $_POST["npass"] == null or $_POST["npass2"] == null){
-	//header("location: conta.php?pass=erro");
+	header("location: conta.php?pass=erro");
 }
 $oldPass = $_POST["pass"];
 $newPass = $_POST["npass"];
+
 if(strcasecmp($pass,$oldPass)!=0){
 	pg_close($conn);
-	//header("location: conta.php?pass=passDiferente");
+	header("location: conta.php?pass=passDiferente");
 }
-echo("</br>antiga: ".strcasecmp($pass,$oldPass));
+
 if(strcasecmp($newPass,$oldPass)==0){
 	pg_close($conn);
-	//header("location: conta.php?pass=passOldigual");
+	header("location: conta.php?pass=passOldigual");
 }
-echo("</br>nova  e velha: ".strcasecmp($newPass,$oldPass));
+
 if(strcasecmp($newPass,$_POST["npass2"])!=0){
 	pg_close($conn);
-	//header("location: conta.php?pass=passdiferente");
+	header("location: conta.php?pass=passdiferente");
 }
-echo("</br>novas iguais: ".strcasecmp($newPass,$_POST["npass2"]));
+
 $sql = "UPDATE \"utilizador\" SET \"pass\" = '$newPass' WHERE \"user\" = '$user'";
-echo("</br>".$sql);
+
 if(pg_query($conn,$sql)){
 	pg_close($conn);
-	//header("location: conta.php?pass=success");
+	header("location: conta.php?pass=success");
 }else{
 	pg_close($conn);
-	//header("location: conta.php?pass=falhou");
+	header("location: conta.php?pass=falhou");
 }
 ?>
