d89417002b0688507308c6829e42587a06352f18
Herberto Tiago Baptista Cardoso <htbc@dei.student.uc.pt>
2018-01-07 18:37:53 +0000

    msn

--- web/newPassword.php
+++ web/newPassword.php
@@ -25,16 +25,6 @@ if($_POST["pass"] == null or $_POST["npass"] == null or $_POST["npass2"] == null
 }
 $old_pass = $_POST["pass"];
 $new_pass = $_POST["npass"];
-
-if(!$old_pass===$pass){
-	echo("$old_pass</br>$pass");
-	header("location: conta.php?pass=badpass");
-}
-if(!$_POST["npass"]===$_POST["npass2"]){
-	header("location: conta.php?pass=passdiferente");
-}
-
-//header("location: conta.php?pass=success")
 ?>
 <html>
 <head>
