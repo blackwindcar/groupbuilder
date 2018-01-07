c3acb544369cf9e5e4d78391983103e2c263038a
Herberto Tiago Baptista Cardoso <htbc@dei.student.uc.pt>
2018-01-07 19:20:25 +0000

    msn

--- web/newPassword.php
+++ web/newPassword.php
@@ -1,5 +1,7 @@
 <!doctype html>
 <?php
+session_start();
+
 $conn = pg_connect();
 $conn = pg_connect("host=ec2-184-73-206-155.compute-1.amazonaws.com dbname=dcddreidjnggtn user=agvlrlhnpncwkh password=548d6de5795bf901f45d996069017c9002b8374deada3c37331b2ab74bea0e56");
 if (!$conn) {
