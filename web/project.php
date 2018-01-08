b566a818c2c9e0facb1425001851aaac221a2ff1
Herberto Tiago Baptista Cardoso <htbc@dei.student.uc.pt>
2018-01-08 00:35:11 +0000

    msn

---
+++ web/project.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<?php
+// Start the session
+session_start();
+
+$conn = pg_connect();
+$conn = pg_connect("host=ec2-184-73-206-155.compute-1.amazonaws.com dbname=dcddreidjnggtn user=agvlrlhnpncwkh password=548d6de5795bf901f45d996069017c9002b8374deada3c37331b2ab74bea0e56");
+if (!$conn) {
+  echo "An error occured.\n";
+  exit; // Para a execução do script
+}
+
+if($_SESSION["utilizador"]==null or $_SESSION["password"]==null){
+		
+		pg_close($conn);
+		header("location: login.php");
+		
+	}
+$user = $_SESSION["utilizador"];
+$pass = $_SESSION["password"];
+$sql = "select count(*) from \"utilizador\" where \"user\" = '$user' and \"password\" = '$pass'";
+if(pg_fetch_row(pg_query($conn,$sql))[0]=="0"){
+	pg_close($conn);
+	header("location: login.php");
+}
+?>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Projeto</title>
+</head>
+
+<body>
+
+<a href="logout.php">Sair</a>
+</body>
+</html>
