0fbcee1edb2b5ab679536fa2b9a12d2b693b79fd
Herberto Tiago Baptista Cardoso <htbc@dei.student.uc.pt>
2018-01-10 19:52:12 +0000

    msn

--- web/project.php
+++ web/project.php
@@ -44,7 +44,7 @@ if(pg_fetch_row(pg_query($conn,$sql))[0]=="1"){
 }else{
 	$grupo = false;
 }
-$sql = "select count(*) from \"grupo\",\"uestap\" where \"grupoid\" = \"id\" and \"utilizadoruser\" = '$user' and grupo.projetonome = '$nome and grupo.projetoadmin = '$user'";
+$sql = "select count(*) from \"grupo\",\"uestap\" where \"grupoid\" = \"id\" and \"utilizadoruser\" = '$user' and grupo.projetonome = '$nome' and grupo.projetoadmin = '$user'";
 echo($sql);
 if(pg_fetch_row(pg_query($conn,$sql))[0]=="1"){
 	$grupoAdmin = true;
