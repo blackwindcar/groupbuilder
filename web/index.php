fb3184d9878c3dddd931e485414ae9b39a1818f9
Herberto Tiago Baptista Cardoso <htbc@dei.student.uc.pt>
2018-01-15 12:46:41 +0000

    msn

--- web/index.php
+++ web/index.php
@@ -46,7 +46,7 @@ if(pg_fetch_row(pg_query($conn,$sql))[0]=="0"){
 		</div>
 		<div class="col-sm-4 text-right">
 			<h3>
-				<a href="conta.php"><?php echo($user); ?></a>
+				<a href="conta.php" class="btn botao-form btn-primary"><?php echo($user); ?></a>
 			</h3>
 		</div>
 	</div>
